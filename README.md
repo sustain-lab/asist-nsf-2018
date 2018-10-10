9d39f6d33a4efbe813957b6c912b3032b9b8ddf7
Milan Curcic <caomaco@gmail.com>
2018-10-10 13:54:42 -0400

    fixes to setup parameters

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ASIST-NSF-2018
 
 Processing data from the ASIST-NSF-2018 experiment
+
+## Getting started
+
+```
+pip install --process-dependency-links git+https://github.com/sustain-lab/asist-nsf-2018
+```
--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author_email='caomaco@gmail.com',
     url='https://github.com/sustain-lab/asist-nsf-2018',
     packages=['asist_nsf_2018'],
-    install_requires=['asist-python'],
-    dependency_links=['git+https://github.com/sustain-lab/asist-python.git@master#egg=asist-0.1.0'],
+    install_requires=['asist-python==0.1.0'],
+    dependency_links=['git+https://github.com/sustain-lab/asist-python.git@master#egg=asist-python-0.1.0'],
     test_suite='asist_nsf_2018.tests',
     license='MIT'
 )
